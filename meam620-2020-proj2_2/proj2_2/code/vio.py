d0427d87c6750994ea5c34d9db6cff61a3b8d379
Yupeng Li <iamliguangming@gmail.com>
2020-04-28 22:02:34 -0400

    Passed all the tests except first one

--- meam620-2020-proj2_2/proj2_2/code/vio.py
+++ meam620-2020-proj2_2/proj2_2/code/vio.py
@@ -20,7 +20,12 @@ def nominal_state_update(nominal_state, w_m, a_m, dt):
     :return: new tuple containing the updated state
     """
     # Unpack nominal_state tuple
-     p, v, q, a_b, w_b, g = nominal_state
+    p, v, q, a_b, w_b, g = nominal_state
+    R = q.as_matrix()
+    new_p = p + v * dt + 1/2 * (R @ (a_m - a_b) + g) * dt ** 2
+    new_v = v + (R @ (a_m - a_b) + g) * dt 
+    r_2= Rotation.from_rotvec(((w_m - w_b) * dt).flatten())
+    new_q = q * r_2
 
     # YOUR CODE HERE
 
@@ -48,6 +53,34 @@ def error_covariance_update(nominal_state, error_state_covariance, w_m, a_m, dt,
 
     # Unpack nominal_state tuple
     p, v, q, a_b, w_b, g = nominal_state
+    Fx = np.zeros((18,18))
+    Fx[0:3,0:3] = np.identity(3)
+    Fx[0:3,3:6] = np.identity(3) * dt
+    Fx[3:6,3:6] = np.identity(3)
+    delta_a = a_m - a_b
+    Fx[3:6,6:9] = -q.as_matrix() @ np.array([[0,-delta_a[2],delta_a[1]],
+                                           [delta_a[2],0,-delta_a[0]],
+                                           [-delta_a[1],delta_a[0],0]]) * dt
+    Fx[6:9,6:9] = q.as_matrix().T @ Rotation.from_rotvec(((w_m -w_b)*dt).flatten()).as_matrix()
+    Fx[3:6,9:12] = -q.as_matrix() * dt
+    Fx[3:6,15:18] = np.identity(3) * dt
+    Fx[6:9,12:15] = - np.identity(3)*dt
+    Fx[9:18,9:18] = np.identity(9)
+    
+    Fi = np.zeros((18,12))
+    Fi[3:15,0:12] = np.identity(12)
+    Qi = np.zeros((12,12))
+    Qi[0:3,0:3] = accelerometer_noise_density ** 2 * dt**2 *np.identity(3)
+    Qi[3:6,3:6] = gyroscope_noise_density ** 2 * dt ** 2 * np.identity(3)
+    Qi[6:9,6:9] = accelerometer_random_walk ** 2 * dt * np.identity(3)
+    Qi[9:12,9:12] = gyroscope_random_walk ** 2 * dt * np.identity(3)
+    
+    error_state_covariance = Fx @ error_state_covariance @ Fx.T + Fi @ Qi @ Fi.T
+    
+    return error_state_covariance
+    
+    
+    
 
     # YOUR CODE HERE
 
@@ -71,6 +104,35 @@ def measurement_update_step(nominal_state, error_state_covariance, uv, Pw, error
     
     # Unpack nominal_state tuple
     p, v, q, a_b, w_b, g = nominal_state
+    (Xc, Yc, Zc) = (q.as_matrix().T @ (Pw - p)).flatten()
+    innovation = uv - 1/Zc * np.array([[Xc],[Yc]])
+    if np.linalg.norm(innovation) >= error_threshold:
+            return (p, v, q, a_b, w_b, g), error_state_covariance, innovation
+    Pc_0 = q.as_matrix().T @ (Pw - p)
+    del_zt_del_Pc = np.array([[1/Zc,0,-Xc/Zc**2],
+                              [0,1/Zc,-Yc/Zc**2]])
+    Pc_0_mat = np.array([[0,-Pc_0[2],Pc_0[1]],
+                         [Pc_0[2],0,-Pc_0[0]],
+                         [-Pc_0[1],Pc_0[0],0]])
+    H = np.zeros((2,18))
+    H[0:2,0:3] = del_zt_del_Pc @ (-q.as_matrix().T)
+    H[0:2,6:9] = del_zt_del_Pc @ Pc_0_mat
+    K_t = error_state_covariance @ H.T @ np.linalg.inv(H 
+                                          @ error_state_covariance 
+                                          @ H.T + Q)
+    
+    delta_x = K_t @ innovation
+    p = p + delta_x[0:3]
+    v = v + delta_x[3:6]
+    q = Rotation.from_euler('XYZ',q.as_euler('XYZ')+ delta_x[6:9].flatten())
+    a_b = a_b + delta_x[9:12]
+    w_b = w_b + delta_x[12:15]
+    g = g + delta_x[15:18]
+    I = np.identity(18)
+    error_state_covariance = (I - K_t @ H) @ error_state_covariance @ (
+        I - K_t @ H).T + K_t @ Q @ K_t.T
+    
+    
 
     # YOUR CODE HERE - compute the innovation next state, next error_state covariance
     
